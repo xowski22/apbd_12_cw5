5b518d9df6c58f3867da4a7b2a8e117ae9fa6aa6
xowski22 <xowski22@gmail.com>
2025-05-18 18:11:06 +0200

    bug fixes

--- WebApplication1/Controllers/PresciptionsController.cs
+++ WebApplication1/Controllers/PresciptionsController.cs
@@ -5,7 +5,43 @@ using WebApplication1.Services;
 using WebApplication1.DTOs;
 namespace WebApplication1.Controllers;
 
-public class PresciptionsController
+[Route("api/[controller]")]
+[ApiController]
+public class PresciptionsController : ControllerBase
 {
-    
+    private readonly IPrescriptionService _prescriptionService;
+
+    public PresciptionsController(IPrescriptionService prescriptionService)
+    {
+        _prescriptionService = prescriptionService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddPrescription(AddPrescriptionDto prescriptionDto)
+    {
+        try
+        {
+            var prescriptionId = await _prescriptionService.AddPrescriptionAsync(prescriptionDto);
+            return CreatedAtAction(nameof(GetPatients), new { idPatient = prescriptionDto.Patient.IdPatient}, prescriptionDto);
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "Wystąpił błąd podczas dodania recepty.");
+        }
+    }
+
+    [HttpGet("patients/{IdPatient}")]
+    public async Task<IActionResult> GetPatients(int IdPatient)
+    {
+        var patientDetails = await _prescriptionService.GetPatientDetailsAsync(idPatient : IdPatient);
+        
+        if (patientDetails == null)
+            return NotFound($"Pacjent o ID {IdPatient} nie istnieje.");
+        
+        return Ok(patientDetails);
+    }
 }
--- WebApplication1/DTOs/PrescriptionMedicamentDto.cs
+++ WebApplication1/DTOs/PrescriptionMedicamentDto.cs
@@ -6,5 +6,6 @@ public class PrescriptionMedicamentDto
     public string Name { get; set; }
     public string Description { get; set; }
     public int Dose { get; set; }
+    public string Details { get; set; }
     
 }
